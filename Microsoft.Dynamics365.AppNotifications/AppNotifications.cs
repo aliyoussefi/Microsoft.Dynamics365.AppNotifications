dcf9aeae296f651151a60541ad6ab5099f386375
Ali Youssefi <alyousse@microsoft.com>
2021-08-31 11:33:49 -0500

    added broadcast to app notifications

--- Microsoft.Dynamics365.AppNotifications/AppNotifications.cs
+++ Microsoft.Dynamics365.AppNotifications/AppNotifications.cs
@@ -1,17 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using Microsoft.PowerPlatform.Dataverse.Client;
 using Microsoft.Xrm.Sdk;
-using Microsoft.Xrm.Sdk.Client;
-using Microsoft.Xrm.Sdk.Query;
 using Newtonsoft.Json;
+using System;
+using System.Net;
 
 namespace Microsoft.Dynamics365.AppNotifications
 {
@@ -21,6 +15,7 @@ namespace Microsoft.Dynamics365.AppNotifications
         public readonly string orgUrl;
         public readonly string clientId;
         public readonly string clientSecret;
+        public readonly string conn;
         public AppNotifications()
         {
             this.tenantId = System.Environment.GetEnvironmentVariable(
@@ -31,57 +26,39 @@ namespace Microsoft.Dynamics365.AppNotifications
                                     "CLIENT_ID", EnvironmentVariableTarget.Process);
             this.clientSecret = System.Environment.GetEnvironmentVariable(
                                     "CLIENT_SECRET", EnvironmentVariableTarget.Process);
+            this.conn = System.Environment.GetEnvironmentVariable(
+                        "CONNECTION_STRING", EnvironmentVariableTarget.Process);
 
         }
-        [Function("CreateAppNotification")]
-        public HttpResponseData CreateAppNotification([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+        [Function("BroadcastAppNotification")]
+        public HttpResponseData BroadcastAppNotification([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
             FunctionContext executionContext)
         {
-            var logger = executionContext.GetLogger("Function1");
-            logger.LogInformation("C# HTTP trigger function processed a request.");
+            var logger = executionContext.GetLogger("BroadcastAppNotification");
 
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions!");
-
-            foreach (var header in req.Headers)
-            {
-                //log.LogInformation(header.Key, header.Value);
-            }
-
-            //var content = req..Body;
             string jsonContent = req.ReadAsStringAsync().Result;
             dynamic data = JsonConvert.DeserializeObject(jsonContent);
             string title = data?.title;
-            DateTimeOffset timestamp = DateTime.Now;
             string body = data?.body;
             string icontype = data?.icontype;
             string toasttype = data?.toasttype;
 
-            string token = ConnectToDynamics(new S2SAuthenticationSettings()
+            DAL.Dataverse dataverse = new DAL.Dataverse(new DAL.Dataverse.ConnectionSettings()
             {
                 clientId = clientId,
                 clientSecret = clientSecret,
+                organizationUrl = orgUrl,
                 tenantID = tenantId,
-                organizationUrl = orgUrl
+                serviceClientConnectionString = conn
             });
-            string conn = System.Environment.GetEnvironmentVariable(
-                                    "CONNECTION_STRING", EnvironmentVariableTarget.Process);
 
-            ServiceClient client = GetNewCrmServiceClient(conn);
+            ServiceClient client = dataverse.GetNewCrmServiceClient(false);
 
+            EntityCollection ec = dataverse.QuerySystemUsers(client);
 
-            List<Entity> rtnObject = new List<Entity>();
-            QueryExpression qx = new QueryExpression("systemuser");
-            qx.ColumnSet.AllColumns = true;//.AddColumn("fullname");
-
-            //qx.TopCount = top;
-            //qx.NoLock = nolock;
-            //qx.Distinct = distinct;
-            qx.Criteria = new FilterExpression();
-            qx.Criteria.AddCondition("accessmode" , ConditionOperator.Equal , 0);
-            EntityCollection ec = client.RetrieveMultiple(qx);
             foreach (Entity user in ec.Entities)
             {
                 Entity appNotification = new Entity("appnotification");
@@ -91,51 +68,19 @@ namespace Microsoft.Dynamics365.AppNotifications
                 appNotification.Attributes.Add("ownerid", new EntityReference("systemuser", user.Id));
                 appNotification.Attributes.Add("icontype", icontype);
                 appNotification.Attributes.Add("toasttype", toasttype);
-                client.Create(appNotification);
+                try
+                {
+                    dataverse.CreateRecord(client, appNotification);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, ex.Message);
+                }
+
             }
-
-
-
-            //RequestModel objRequestModel = new RequestModel();
-            //objRequestModel.correlationid = data?.correlationId ?? exCtx.InvocationId.ToString();
-            //HttpResponseMessage rtnObject = req.CreateResponse(HttpStatusCode.OK, objRequestModel);
-            //rtnObject.Headers.Add("InvocationId", exCtx.InvocationId.ToString());
-            //return rtnObject;
-
             return response;
         }
 
-        private ServiceClient GetNewCrmServiceClient(string conn)
-        {
-            OrganizationServiceContext orgContext;
-            PowerPlatform.Dataverse.Client.ServiceClient client = new PowerPlatform.Dataverse.Client.ServiceClient(conn);
-            //CrmServiceClient client = new CrmServiceClient(conn);
-            //if (client == null || client.OrganizationServiceProxy == null)
-            //{
-            //    client = new CrmServiceClient(conn);
-            //}
 
-            orgContext = new OrganizationServiceContext(client);
-
-
-            return client;
-        }
-
-        private string ConnectToDynamics(S2SAuthenticationSettings authenticationSettings)
-        {
-            ClientCredential clientcred = new ClientCredential(authenticationSettings.clientId, authenticationSettings.clientSecret);
-            AuthenticationContext authenticationContext = new AuthenticationContext(authenticationSettings.aadInstance + authenticationSettings.tenantID);
-            var authenticationResult = authenticationContext.AcquireTokenAsync(authenticationSettings.organizationUrl, clientcred).Result;
-            return authenticationResult.AccessToken;
-
-        }
-        public class S2SAuthenticationSettings
-        {
-            public string organizationUrl;
-            public string clientId;
-            public string clientSecret;
-            public string aadInstance = "https://login.microsoftonline.com/";
-            public string tenantID;
-        }
     }
 }
---
+++ Microsoft.Dynamics365.AppNotifications/DAL/Dataverse.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.PowerPlatform.Dataverse.Client;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Dynamics365.AppNotifications.DAL
+{
+    public class Dataverse
+    {
+        private ConnectionSettings _conn;
+        public class ConnectionSettings
+        {
+            public string organizationUrl;
+            public string clientId;
+            public string clientSecret;
+            public string aadInstance = "https://login.microsoftonline.com/";
+            public string tenantID;
+            public string serviceClientConnectionString;
+        }
+        public Dataverse(ConnectionSettings settings)
+        {
+            _conn = settings;
+        }
+
+        private string ConnectToDynamics(ConnectionSettings authenticationSettings)
+        {
+            ClientCredential clientcred = new ClientCredential(authenticationSettings.clientId, authenticationSettings.clientSecret);
+            AuthenticationContext authenticationContext = new AuthenticationContext(authenticationSettings.aadInstance + authenticationSettings.tenantID);
+            var authenticationResult = authenticationContext.AcquireTokenAsync(authenticationSettings.organizationUrl, clientcred).Result;
+            return authenticationResult.AccessToken;
+
+        }
+
+        public ServiceClient GetNewCrmServiceClient(bool useServicePrincipal)
+        {
+            if (useServicePrincipal)
+            {
+                PowerPlatform.Dataverse.Client.ServiceClient client = new PowerPlatform.Dataverse.Client.ServiceClient(new Uri(_conn.organizationUrl), _conn.clientId, _conn.clientSecret, true);
+                return client;
+            }
+            else
+            {
+                PowerPlatform.Dataverse.Client.ServiceClient client = new PowerPlatform.Dataverse.Client.ServiceClient(_conn.serviceClientConnectionString);
+                return client;
+            }
+        }
+
+        public EntityCollection QuerySystemUsers(ServiceClient client)
+        {
+            QueryExpression qx = new QueryExpression("systemuser");
+            qx.ColumnSet.AllColumns = true;//.AddColumn("fullname");
+
+            //qx.TopCount = top;
+            //qx.NoLock = nolock;
+            //qx.Distinct = distinct;
+            qx.Criteria = new FilterExpression();
+            qx.Criteria.AddCondition("accessmode", ConditionOperator.Equal, 0);
+            EntityCollection ec = client.RetrieveMultiple(qx);
+            return ec;
+        }
+
+        public Guid CreateRecord(ServiceClient client, Entity appNotification)
+        {
+            return client.Create(appNotification);
+        }
+    }
+}
